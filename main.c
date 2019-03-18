af61b2764917d42e81417c6bd3f58f3f048be25f
david schouten <david.schouten@hotmail.com>
2019-03-18 21:50:30 +0100

    no comment

--- main.c
+++ main.c
@@ -8,9 +8,10 @@
 //reverse?
 //back emf sensing?
 //sensorless?, automatic fault detection and switching to sensorless, what type sensors?
+//sensor less? -> speed under x? -> just apply a rotating field
 //alu and fpu in parralel?
 //register variables?
-//prolly dont need any context saving in the fpu
+//prolly dont need any context saving in the fpu(set on a per function basis?)
 //s16 and up need to be preserved use those for variables
 //the compiler now pushes d8-d10 can my fpu handle this, have i selected the right one
 //core coupled memory, different ram banks?
@@ -22,13 +23,29 @@
 //backemf sensing on higset priority timer interrupt -> wait for peak and set timer accordingly?
 //data/instruction caching in both core and st implementation?
 //ideally you would request torque from the controller
+//add torque per amp lookup table?
 //ethernet or can?
-//cleanup ifdefs, only if the roots let the compiler remove uncalled code/variables
 //stacksize?
 //init order still allow optimizations?
+//interrupt (subroutine calls?) stacking behavior?
+//interrupt priorities, turn of nestng globally? (i would be nice if advance tracking could be interrupted though)
+//max speed per amp vs max speed per energy?
+//peak detection once per x rotations?
+//calculate speed in peak detection?
+//only 4 32 bit instructions of prefetch, 5 waitstates->problem?
+//also check core prefetch for sequential 32 bit instructions
+//io compensation cell?
+//divide by zero handling?
+//dsp instuctions!(?)
+//stack in ram(does the cpu always lock out the dma when stacking)?
+//exc_return?(in arm refman)
+//mpu?
 
 //defines
-#define min_mag_commutation 1
+#define CCMRAM __attribute__((section(".ccmram")))
+
+#define min_err_commutation 1
+#define peak_detection_window 1
 #define n_samples_averaging 1
 #define sin60 0.8660254
 #define sin120 sin60
@@ -37,8 +54,8 @@
 #define default_can_adress something
 #define prop_delay 0
 #define current_per_count 1
-#define current_count_midpoint 2048
-//#define cheat_at_mtpa //make the gross assumption that load is constant and therefore max speed corresponds to max torque
+#define current_midpoint 50
+//#define track_max_speed_per_power
 
 //typedefs
 typedef struct {
@@ -66,9 +83,13 @@ extern uint32_t _sdata;
 extern uint32_t _edata;
 extern uint32_t _sbss;
 extern uint32_t _ebss;
+extern uint32_t _sccmram;
+extern uint32_t _svtflash;
+extern uint32_t _evtflash;
+
 
 //constants, const gets placed in text! can be changed in linker(relation to literals?)
-#ifndef cheat_at_mtpa
+#ifndef track_max_speed_per_power
 const float mtpa_lut[][1];//datatype?
 #endif
 
@@ -119,29 +140,26 @@ void process_data(void);
 vector_mag_ang calculate_desired_current(void);
 void svm_correct_current_towards(vector_mag_ang);
 void stator_field_controll(void);
-void track_max_speed_per_amp(void);
+void optimize_advance_angle(void);
 
 extern void TIM2_IRQHandler(void);
 extern void TIM3_IRQHandler(void);
-#ifdef cheat_at_mtpa
 extern void TIM4_IRQHandler(void);
-#endif
 
 void init_system(){
 	//setup system clocks
-	//no reset to initial state for now
 	//enable HSE
-	RCC->CR = RCC_CR_HSEON;
+	RCC->CR = RCC_CR_HSION & RCC_CR_HSEON;
 	//wait till HSE is ready
 	while ((RCC->CR & RCC_CR_HSERDY) == 0){}
 	//configure art accelerator?
 	//configure the Flash Latency cycles and enable prefetch buffer
-	//FLASH->ACR;
-	//configure the HCLK, ahb, apb prescalers
-	//RCC->CFGR;
+	FLASH->ACR = FLASH_ACR_LATENCY_5WS & FLASH_ACR_ICEN & FLASH_ACR_ICEN & FLASH_ACR_PRFTEN;
+	//configure the ahb, apb prescalers
+	RCC->CFGR = RCC_CFGR_PPRE1_DIV2 & RCC_CFGR_PPRE2_DIV2;//delay in completion!
 	//configure and enable PLL
-	RCC->PLLCFGR = (7<<RCC_PLLCFGR_PLLQ) & (1<<RCC_PLLCFGR_PLLSRC) & (168<<RCC_PLLCFGR_PLLN) & 4;
-	RCC->CR = RCC_CR_PLLON;
+	RCC->PLLCFGR = (7<<24) & RCC_PLLCFGR_PLLSRC & (168<<6) & 4;
+	RCC->CR |= RCC_CR_PLLON;
 	//wait till PLL is ready
 	while ((RCC->CR & RCC_CR_PLLRDY) == 0){}
 	//select PLL as system clock source
@@ -149,15 +167,12 @@ void init_system(){
 	//wait till PLL is used as system clock source
 	while ((RCC->CFGR & RCC_CFGR_SWS) != RCC_CFGR_SWS_PLL){}
 
-	//init ram
-	uint32_t *src, *dst;
 	//copy the data segment into ram
+	uint32_t *src, *dst;
 	src = &_sidata;
 	dst = &_sdata;
-	if (src != dst){
-		while(dst < &_edata){
-			*(dst++) = *(src++);
-		}
+	while(dst < &_edata){
+		*(dst++) = *(src++);
 	}
 	//zero the bss segment
 	dst = &_sbss;
@@ -165,30 +180,70 @@ void init_system(){
 		*(dst++) = 0;
 	}
 
-	//setup debug?
-	//setup fpu, load freq used constants ?
+	//setup fpu, load freq used constants
+	SCB->CPACR = 0b1111<<20;
+	//asm volatile("vmov.f32 s5, #0");
+	//synch barriers?
 	//setup eeprom emulation?
 	//configure peripheral clocks
-	RCC->AHB1ENR = 1<<22;//enable dma2, gpio clocks,no |=?
-	RCC->APB1ENR = 0;//enable timx
-	RCC->APB2ENR = 1<<8 | 1;//enable adc1, usart, tim1 clocks
+	RCC->AHB1ENR = RCC_AHB1ENR_GPIOAEN & RCC_AHB1ENR_CCMDATARAMEN & RCC_AHB1ENR_DMA2EN;
+	RCC->APB1ENR = RCC_APB1ENR_TIM2EN & RCC_APB1ENR_TIM3EN & RCC_APB1ENR_TIM4EN;
+	RCC->APB2ENR = RCC_APB2ENR_TIM1EN & RCC_APB2ENR_USART1EN & RCC_APB2ENR_ADC1EN & RCC_APB2ENR_ADC2EN;
 
-	//turn on/off peripherals
+	//copy vector table to ccram
+	src = &_svtflash;
+	dst = &_sccmram;
+	while(src < &_evtflash){
+		*(dst++) = *(src++);
+	}
+
+	//relocate vector table to ccram
+	SCB->VTOR = &_sccmram;
+
+	//turn on/off peripherals?
+
 	//setup gpio
-	//GPIOA->MODER;
-	//RCC->CR;
-	//setup adc
-	//setup dma
-	//setup ethernet
+//	GPIOA->MODER;//function
+//	GPIOA->OTYPER;//output type
+//	GPIOA->OSPEEDR;
+//	GPIOA->PUPDR;//pullups
+//	GPIOA->AFR;//alternate function select
+
 	//setup usart?
-	//negtiote parameters with main controller
+	//setup adc
+	ADC1->CR1 = (0b00<<24) & ADC_CR1_AWDIE;//res?, jauto?, single anawd channel(use anawd at all?)
+
+	//setup dma
+
 	//setup encoder?
+
 	//setup tim1 for deadtime
+	TIM1->CR1=0;//set deadtime prescaler
+	TIM1->CCER = TIM_CCER_CC1E & TIM_CCER_CC1NE & TIM_CCER_CC2E & TIM_CCER_CC2NE & TIM_CCER_CC3E & TIM_CCER_CC3NE;//complentary output polarity?
+	TIM1->BDTR = TIM_BDTR_MOE ;//set deadtime
+
 	//setup 40khz loop on tim2
+	TIM2->CR1 = TIM_CR1_CEN;
+	TIM2->DIER = TIM_DIER_UIE;
+	TIM2->EGR = TIM_EGR_UG;
+	TIM2->PSC = 10;
+	TIM2->CCR1 = 10;
+
 	//setup stator field timer on tim3?
-	#ifdef cheat_at_mpta
-		//setup best advance tracking
-	#endif
+	TIM3->CR1 = TIM_CR1_CEN;
+	TIM3->DIER = TIM_DIER_UIE;
+	TIM3->EGR = TIM_EGR_UG;
+	TIM3->PSC = 10;
+	TIM3->CCR1 = 10;
+
+#ifdef track_max_speed_per_power
+	//setup best advance tracking on tim4?
+#endif
+
+	//setup ethernet/can
+
+	//negtiote parameters with main controller
+
 	//setup interrupts, priorities
 	//NVIC->ISER;
 
@@ -204,23 +259,22 @@ void main(){
 void process_data(){
 	//filters and averaging?
 	//decode values
-	i_a = (((float)adc_circ_buffer[0].i_a) - current_count_midpoint) * current_per_count;
-	i_b = (((float)adc_circ_buffer[0].i_b) - current_count_midpoint) * current_per_count;
-	rotor_e_pos = finvtan2(adc_circ_buffer[0].v_rotor_x, adc_circ_buffer[0].v_rotor_y) * n_polepairs_per_poles_sense_magnet;//wraparound?
+	i_a = ((float)adc_circ_buffer[0].i_a) * current_per_count - current_midpoint;
+	i_b = ((float)adc_circ_buffer[0].i_b) * current_per_count - current_midpoint;
+	rotor_e_pos = finvtan2(adc_circ_buffer[0].v_rotor_x, adc_circ_buffer[0].v_rotor_y) * n_polepairs_per_poles_sense_magnet;//have arctan output 32bit fixed point keep it there till svm. does wraparound also work like this on multiplication?
 	//adjusts
 	rotor_e_pos = query_encoder_table(rotor_e_pos);
-	speed = rotor_e_pos - prev_rotor_e_pos;//wraparound?
+	speed = rotor_e_pos - prev_rotor_e_pos;
 	prev_rotor_e_pos = rotor_e_pos;
-	rotor_e_pos = rotor_e_pos + speed * prop_delay;//this can be incoporated in the mpta lut(?)
+    //rotor_e_pos = rotor_e_pos + speed * prop_delay;//adjust for propogation delay, this can be incoporated in the mpta lut(?), do it at all?
 }
 
 void stator_field_controll(){
 	//no contex saving on fpu? -> do manually here
-	//100 herz?
 
 }
 
-void track_max_speed_per_amp(){
+void optimize_advance_angle(){
 	//no contex saving on fpu? -> do manually here
 	//100 herz?
 
@@ -281,9 +335,10 @@ void svm_correct_current_towards(vector_mag_ang ref_current){
 
 	//pass best to deadtime gen
 	//init override bits?
-	uint16_t table[]={0b0100000001010000, 0b0101000001010000, 0b0101000001000000, 0b0101000001000000, 0b0100000001000000, 0b0100000001010000};
-	TIM1->CCMR1=table[index_best];
-	//other register!
+	uint16_t table0[]={0b0100000001010000, 0b0101000001010000, 0b0101000001000000, 0b0101000001000000, 0b0100000001000000, 0b0100000001010000};
+	uint16_t table1[]={0b0100000001000000, 0b0100000001000000, 0b0100000001000000, 0b0100000001010000, 0b0100000001010000, 0b0100000001010000};
+	TIM1->CCMR1=table0[index_best];
+	TIM1->CCMR2=table1[index_best];
 }
 
 void calibrate_encoder(){
@@ -321,10 +376,10 @@ void TIM3_IRQHandler(){
 	stator_field_controll();
 }
 
-#ifdef cheat_at_mtpa
+#ifdef track_max_speed_per_power
 void TIM4_IRQHandler(){
 	//interrupt priority?
 	//dma?
-	track_max_speed_per_amp();
+	optimize_advance_angle();
 }
 #endif
